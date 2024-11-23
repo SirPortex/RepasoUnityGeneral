f8d512f7e84f98216f61877609468035c338a1bd
Pavel.K144 <portex144@gmail.com>
2024-11-23 12:16:45 +0100

    Enemy

---
+++ Assets/Scripts/AI/ChaseState.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(fileName = "ChaseState (S)", menuName = "ScriptableObject/States/ChaseState")] //IMPORTANTE
+
+public class ChaseState : State
+{
+    public override State Run(GameObject owner)
+    {
+        State nextState = CheckActions(owner);
+        NavMeshAgent navMeshAgent = owner.GetComponent<NavMeshAgent>(); // al herederar de ScriptableObject hay que poner owner para poder heredar el componente de NavMeshAgent
+        GameObject target = owner.GetComponent<TargetReference>().target; // indentificamos cual es el target
+
+
+        navMeshAgent.SetDestination(target.transform.position); // tu destino es el target, ve a por el.
+
+
+        return nextState;
+    }
+}
---
+++ Assets/Scripts/AI/SeenAction.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SeenAction (A)", menuName = "ScriptableObject/Actions/SeenAction")]
+
+public class SeenAction : Action
+{
+    public override bool Check(GameObject owner)
+    {
+        GameObject target = owner.GetComponent<TargetReference>().target;
+        ConeCollider collider = target.GetComponentInChildren<ConeCollider>();
+        List<GameObject> targetViewList = collider.list;
+
+        foreach (GameObject objectInVision in targetViewList)
+        {
+            if (owner == objectInVision)
+            {
+                Debug.Log("UN ENEMIGO DEL FORTNITE");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public override void DrawGizmos(GameObject owner)
+    {
+
+    }
+}
